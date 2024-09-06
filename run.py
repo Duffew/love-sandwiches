d4045baddb737b63cd4669c833465f9dc5a9fccf
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-09-06 10:13:54 +0000

    Add user request loop to repeat request for data until valid data is provided

--- run.py
+++ run.py
@@ -17,22 +17,31 @@ def get_sales_data():
     """
     get sales figures from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be 6 numbers separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
 
-    # next use the input method to get our sales data from the terminal
-    data_str = input("Enter your data here: ")
+    # create a 'while' loop to prevent having to relaunch the programme
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be 6 numbers separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        # next use the input method to get our sales data from the terminal
+        data_str = input("Enter your data here: ")
     
-    # convert the user's string input data to a list 
-    # and use the split() method - returns the broken up values as a list -  
-    # to remove all the commas
+        # convert the user's string input data to a list 
+        # and use the split() method - returns the broken up values as a list -  
+        # to remove all the commas
+        sales_data = data_str.split(",")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        if validate_data(sales_data): # if statement calls the validate_data() function and if there are no errors, returns True
+            print("Data looks good")
+            break
+
+    # now return out validated sales_data from the get_sales_data() function
+    return sales_data
+        
 
 # create a function to handle data validation - we need exactly 6 numbers
-# call the function in the get_sales_data() function above
+# call the validate_data() function in the get_sales_data() function above
 def validate_data(values):
     """
     Inside the try, converts all string values to integers.
@@ -53,7 +62,10 @@ def validate_data(values):
         
     except ValueError as e:
             print(f"Invalid data: {e}, please try again.\n")
+            return False # if error
+
+    return True # if no error
 
 
 
-get_sales_data()
+data = get_sales_data()
